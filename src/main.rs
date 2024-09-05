938b723eda8c6ecf619303fc7a96f84ea2facc2b
andersbohnm <andersbohn@gmail.com>
2024-09-05 17:06:33 +0200

    activity typed

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,20 @@
 mod gcdomain;
 
 use std::fs;
-
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-struct Activity {
-    name: String,
-    age: u8,
-    phones: Vec<String>,
-}
-
+use gcdomain::Activity;
 
 fn main() {
     println!("Hello, world!");
-    let path = "./gc-1972-01-01-2024-09-05.json";
+    let path = "./resources/local/gc-1972-01-01-2024-09-05.json";
     let data = fs::read_to_string(path);
     match data {
         Ok(jsonStr) => {
-        let res = serde_json::from_str::<serde_json::Value>(&jsonStr);
+        let res = serde_json::from_str::<Vec<Activity>>(&jsonStr);
         match res {
             Ok(json) => {
-                println!("amazing json {}", json);
+                let prettyJson = serde_json::to_string_pretty(&json).expect("badpretty");
+                let chopped  = &prettyJson[..100];
+                println!("amazing json {}",chopped);
             }
             Err(bad) => {
                 println!("baad json !! {}", bad);
