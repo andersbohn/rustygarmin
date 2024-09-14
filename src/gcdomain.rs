d5db73e0cd315273a881accba3b34c123c29c3d9
andersbohnm <andersbohn@gmail.com>
2024-09-14 14:40:08 +0200

    working serde with untagged

--- src/gcdomain.rs
+++ src/gcdomain.rs
@@ -1,10 +1,8 @@
 use serde::{de::Visitor, Deserialize, Deserializer,Serialize};
 
 
-
 #[derive(Serialize,Deserialize)]
-// #[serde(tag = "activityType.typeKey")]
-#[serde(tag = "atype")]
+#[serde(untagged)]
 pub enum Activity {
     #[serde(rename = "running")]
     Running {
@@ -43,7 +41,7 @@ pub enum Activity {
         #[serde(rename = "activityName")]
         name: String,
         duration: f64,
-    },#[serde(rename = "strength_training")]
+    },#[serde(rename = "StrengthTraining")]
     Strength {
         #[serde(rename = "activityId")]
         id: u64,
@@ -69,74 +67,94 @@ pub enum Activity {
     },
     #[serde(rename = "cross_country_skiing_ws")]
     CrossCountrySkiingWs {
-        #[serde(rename = "activityId")]
-        id: u64,
         #[serde(rename = "activityName")]
         name: String,
-        duration: f64,
     },
-    // indoor_cardio
-    // indoor_climbing
-    // indoor_cycling
-    // indoor_rowing
-    // indoor_running
-    // lap_swimming
-    // mountaineering
-    // multi_sport
-    // obstacle_run
-    // open_water_swimming
-    // other
-    // resort_skiing_snowboarding_ws
-    // rowing_v2
-    // running
-    // stair_climbing
-    // stand_up_paddleboarding_v2
-    // strength_training
-    // trail_running
-    // treadmill_running
+    #[serde(rename = "TreadmillRunning")]
+    TreadmillRunning {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "TrailRunning")]
+    TrailRunning {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "StrengthTraining")]
+    StrengthTraining {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "StandUpPaddleboardingV2")]
+    StandUpPaddleboardingV2 {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "StairClimbing")]
+    StairClimbing {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "RowingV2")]
+    RowingV2 {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "ResortSkiingSnowboardingWs")]
+    ResortSkiingSnowboardingWs {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "OpenWaterSwimming")]
+    OpenWaterSwimming {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "ObstacleRun")]
+    ObstacleRun {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "MultiSport")]
+    MultiSport {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "mountaineering")]
+    Mountaineering {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "lap_swimming")]
+    LapSwimming {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "indoor_running")]
+    IndoorRunning {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "indoor_rowing")]
+    IndoorRowing {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "indoor_cycling")]
+    IndoorCycling {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "indoor_climbing")]
+    IndoorClimbing {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
+    #[serde(rename = "IndoorCardio")]
+    IndoorCardio {
+        #[serde(rename = "activityName")]
+        name: String,
+    },
     Others,
 }
 
-/*struct ActivityVisitor;
-
-impl<'de> Visitor<'de> for ActivityVisitor {
-    type Value = Activity;
-
-    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        formatter.write_str("Activity type")
-    }
-
-    fn visit_map<V>(self, mut map: V) -> Result<Self::Value, V::Error>
-    where
-        V: serde::de::MapAccess<'de>,
-    {
-        let activity_type = map.next_key::<String>()?.ok_or_else(|| serde::de::Error::custom("Missing activity type"))?;
-        let value = map.next_value::<serde_json::Value>()?;
-
-        match activity_type.as_str() {
-            "hiking" => Ok(Activity::Hiking {
-                id: value["activityId"].as_u64().unwrap(),
-                name: value["activityName"].as_str().unwrap().to_string(),
-                distance: value["distance"].as_f64().unwrap(),
-                duration: value["duration"].as_f64().unwrap(),
-            }),
-            "cycling" => Ok(Activity::Cycling {
-                id: value["activityId"].as_u64().unwrap(),
-                name: value["activityName"].as_str().unwrap().to_string(),
-                distance: value["distance"].as_f64().unwrap(),
-                duration: value["duration"].as_f64().unwrap(),
-                // avg_stride_length: value["avgStrideLength"].as_f64().unwrap(),
-            }),
-            _ => Err(serde::de::Error::custom("Unknown activity type")),
-        }
-    }
-}
-
-impl<'de> Deserialize<'de> for Activity {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_any(ActivityVisitor)
-    }
-}*/
